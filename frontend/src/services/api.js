849475c840db8dd37420488c0264d2ea8bfc066b
TommDaniel <daninho.tomm@gmail.com>
2025-07-11 18:52:30 -0300

    connections

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,7 +17,7 @@ function getCookie(name) {
 }
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'http://192.168.1.103:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
